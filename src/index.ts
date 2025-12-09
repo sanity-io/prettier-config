391a92589d67a783a9946db4e41ba2f58268fe24
Cody Olsen <stipsan@gmail.com>
2025-12-09 13:21:05 +0100

    fix!: remove @prettier/plugin-oxc for now

--- src/index.ts
+++ src/index.ts
@@ -27,22 +27,6 @@ const yaml = {
   },
 } satisfies NonNullable<Config['overrides']>[number]
 
-const oxc = {
-  files: ['**/*.{js,mjs,cjs,jsx}'] as const,
-  options: {
-    plugins: ['@prettier/plugin-oxc'] as const,
-    parser: 'oxc',
-  },
-} satisfies NonNullable<Config['overrides']>[number]
-
-const oxcTs = {
-  files: ['**/*.{ts,mts,cts,tsx}'] as const,
-  options: {
-    plugins: ['@prettier/plugin-oxc'] as const,
-    parser: 'oxc-ts',
-  },
-} satisfies NonNullable<Config['overrides']>[number]
-
 const config = {
   ...overridableDefaults,
   printWidth: 100 as const,
@@ -51,7 +35,7 @@ const config = {
   quoteProps: 'consistent',
   bracketSpacing: false,
   plugins: ['prettier-plugin-packagejson'] as const,
-  overrides: [json5, yaml, oxc, oxcTs] as [typeof json5, typeof yaml, typeof oxc, typeof oxcTs],
+  overrides: [json5, yaml] as [typeof json5, typeof yaml],
 } satisfies Config
 
 export default config
